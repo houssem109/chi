91a2b47b3c3d2be690ba145951a89373362114dc
houssem109 <houssembenjaafar105@gmail.com>
2024-11-11 21:11:42 +0100

    add button for form

--- src/pages/home/components/ContactForm.tsx
+++ src/pages/home/components/ContactForm.tsx
@@ -39,6 +39,14 @@ export default function ContactForm() {
             className="md:w-[600px] w-full bg-transparent pl-2 fon py-3 text-white rounded  border-white/20 border-2 font-mono focus:outline-none focus:border-white/40 transition-all focus:pl-4 focus:placeholder:opacity-0 "
           />
         </div>
+        <div className="flex space-x-4 mt-4">
+        <button
+          name="ResetBtn"
+          type="submit"
+          className="text-neutral-200 bg-gray-500 active:scale-100 transition-all bg-gradient-to-tr from-gray-600 to-gray-700 py-2 text-[16px] px-10 font-sans font-semibold rounded-md hover:from-neutral-300 hover:to-neutral-400 hover:text-black shadow-[0px_0px_20px_#6b728088]">
+          submit
+        </button>
+        </div>
       </form>
     </>
   );
